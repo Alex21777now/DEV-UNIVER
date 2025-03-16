e515c8fca733501aeed58370c0134ba8ce5152e4
Alex21777now <banna707@i.ua>
2025-03-16 14:33:08 +0200

    making face

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,42 @@ import MovieList from './Components/MovieList';
 
 function App() {
   return (
-    <div>
-      
-      <MovieList />
-      
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      {/* Horizontal stripe */}
+      <div
+        style={{
+          width: "124vw", // 124
+          height: "260px",
+          backgroundColor: "	#4682B4",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      ></div>
+
+      {/* Vertical stripe */}
+      <div
+        style={{
+          width: "300px",
+          height: "145vh", // 145
+          backgroundColor: "lightblue",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      ></div>
+      {/* Horizontal stripe */}
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "lightgrey",
+          padding: "15px",
+          position: "absolute",
+          top: 260,
+          left: 300, //190
+        }}
+      ><MovieList /></div>
     </div>
   );
 }
