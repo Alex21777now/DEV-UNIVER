7abcae77403538cf3f92fe6694cc84f7c2df90d3
Alex21777now <banna707@i.ua>
2025-03-21 11:45:37 +0200

    do Movies load

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MovieList from './Components/MovieList';
 import styled from "styled-components";
-
+import movieBackground from "./images/Shwartc2.jpg";
 // Адаптивный контейнер
 const Container = styled.div`
   display: flex;
@@ -12,7 +12,7 @@ const Container = styled.div`
   text-align: center;
   min-height: 100vh;
   padding: 20px;
-  background: linear-gradient(to right, #4facfe, #00f2fe);
+  background: linear-gradient(to right, 	#4682B4, #00f2fe);
 
   @media (max-width: 768px) {
     padding: 10px;
@@ -60,10 +60,17 @@ const Button = styled.button`
   border-radius: 5px;
   cursor: pointer;
   margin-top: 20px;
-  transition: 0.3s;
+  
+  transition: all 0.2s ease;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
 
   &:hover {
     background: #0056b3;
+  }
+
+  &:active {
+    transform: scale(0.90); /* Визуальное уменьшение */
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3); /* Уменьшаем тень */
   }
 
   @media (max-width: 768px) {
@@ -75,7 +82,17 @@ const Button = styled.button`
 function App() {
   return (
     <Container>
-      <Title>DEV-UNIVER</Title>
+      <Title>DEV-UNIVER.VERCEL.APP</Title>
+      <img
+     src={movieBackground}
+     alt="Movie Background"
+     style={{
+      margin: '2px 2px 2px 2px',
+      width: '49%',    // Set the width of the image to 100% of the container
+      height: '50%',   // Set the height of the image to 100% of the container
+      objectFit: 'cover' // Use object-fit to cover the container
+             }}
+        />
       <Text>Программы тренировок в зале для начинающих и более опытных спортсменов</Text>
       
       <Button>Общая  #1</Button><br></br>
@@ -84,6 +101,9 @@ function App() {
       <Button style={{ background: "#32CD32" }}>Плечи руки</Button><br></br>
       <Button style={{ background: "#008000" }}>Грудь спина</Button><br></br>
       <Button style={{ background: "#DC143C" }}>Ноги низ</Button><br></br>
+
+      <Text>In the evening get familiar with newly released Movies coming soon</Text>
+      <MovieList />
     </Container>
   );
 }
